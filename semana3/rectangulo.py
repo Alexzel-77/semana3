52b402a23c8d2543f52dba46dd4538b754b9fa28
Alejandro <aezeledon@uamv.edu.ni>
2025-04-06 13:05:44 -0600

    Delete semana3 directory

--- semana3/rectangulo.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Ingresa la base del rectángulo: "))
-altura = float(input("Ingresa la altura del rectángulo: "))
-area = base * altura
-perimetro = (base * 2) + (altura * 2)
-print("Área del rectángulo:", area)
-print("Perímetro del rectángulo:", perimetro)
